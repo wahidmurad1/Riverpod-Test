7e9b8c608dc6877f9d29050cb8b1596d91ad190d
wahidmurad1 <genie.murad@gmail.com>
2023-11-18 13:06:50 +0600

    2nd way of using riverpod only wrap a widget

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,22 +1,41 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:riverpod_project1/main.dart';
+
+// class HomePage extends ConsumerWidget {
+//   const HomePage({super.key});
+
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref) {
+//     final name = ref.watch(nameProvider);
+//     return Container(
+//       color: Colors.white,
+//       child: SafeArea(
+//         top: false,
+//         child: Scaffold(
+//           body: Center(child: Text(name)),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_project1/main.dart';
 
-class HomePage extends ConsumerWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final name = ref.watch(nameProvider);
-    return Container(
-      color: Colors.white,
-      child: SafeArea(
-        top: false,
-        child: Scaffold(
-          body: Center(child: Text(name)),
-        ),
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Consumer(builder: (context, ref, child) {
+          final name = ref.watch(nameProvider);
+          return Text(name);
+        }),
       ),
     );
   }
 }
-
