35ce20662f8c90f3bd26bf40435c36bb87edf489
wahidmurad1 <genie.murad@gmail.com>
2023-11-18 13:18:33 +0600

    comment remove

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,40 +1,20 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-// import 'package:riverpod_project1/main.dart';
-
-// class HomePage extends ConsumerWidget {
-//   const HomePage({super.key});
-
-//   @override
-//   Widget build(BuildContext context, WidgetRef ref) {
-//     final name = ref.watch(nameProvider);
-//     return Container(
-//       color: Colors.white,
-//       child: SafeArea(
-//         top: false,
-//         child: Scaffold(
-//           body: Center(child: Text(name)),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_project1/main.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Consumer(builder: (context, ref, child) {
-          final name = ref.watch(nameProvider);
-          return Text(name);
-        }),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final name = ref.watch(nameProvider);
+    return Container(
+      color: Colors.white,
+      child: SafeArea(
+        top: false,
+        child: Scaffold(
+          body: Center(child: Text(name)),
+        ),
       ),
     );
   }
